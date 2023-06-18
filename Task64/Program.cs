df8cb9329740a6e627c1a81bf96dc5814cbaf0c9
Kikot Ekaterina <kati-kik@yandex.ru>
2023-06-18 18:40:32 +0300

    решена задача 64

---
+++ Task64/Program.cs
@@ -0,0 +1,13 @@
+﻿string printNumbers(int a, int b)
+{
+    if (a == b)
+    return $"{b}";
+    return $",{b}" + printNumbers(a, b - 1);
+}
+
+Console.Clear();
+Console.Write("Введите 1 число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2 число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(printNumbers(m, n));
