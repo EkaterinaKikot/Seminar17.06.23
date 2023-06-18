b75ffa1e58a9c1da66ca2deac5d73a7267a633af
Kikot Ekaterina <kati-kik@yandex.ru>
2023-06-18 18:59:17 +0300

    решена задача 66

---
+++ Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+void Summa (int M, int N, int sum)
+{
+    if (M > N) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (M++);
+    Summa(M, N, sum);
+}
+
+Console.Clear();
+Console.Write("Введите значение M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine()!);
+Summa(M, N, 0);
